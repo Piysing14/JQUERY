b7a7b54d3d649906175f5dfe0af0f7e90b2cb6a3
Piyush Singh <khatipk14@gmail.com>
2025-02-04 19:45:59 +0530

    code

--- myJQUERY.js
+++ myJQUERY.js
@@ -22,5 +22,30 @@ $(document).ready(function () {
       $('#text').fadeOut(200)
   })
 
+  $('#slideup').click(function(){
+    $('#text').slideUp(1000,function(){
+        console.log('Done')
+    })
+   })
+
+  $('#slidedown').click(function(){
+    $('#text').slideDown(1000,function(){
+        console.log('Done')
+    })
+   })
+
+   $('#slidetoggle').click(function(){
+    $('#text').slideToggle(1000,function(){
+        console.log('Done')
+    })
+   })
+
+   //Speed and callback parameters are optional
+
+   $('#text').animate({
+    opacity: 0.6,
+    height:'150 px'
+   },3000)
+  
 
 });
